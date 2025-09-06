61a58767d7466e690ab06925da02c97686142320
Kevin Clancy <kclanc@gmail.com>
2025-09-05 17:38:51 -0700

    added tests for threads and replies models

--- tests/models/replies.rs
+++ tests/models/replies.rs
@@ -1,31 +1,95 @@
+use insta::assert_yaml_snapshot;
 use kboard::app::App;
 use loco_rs::testing::prelude::*;
 use serial_test::serial;
+use kboard::{
+    models::replies::Model as RepliesModel,
+    models::replies::Entity as ReplisEntity
+};
 
 macro_rules! configure_insta {
     ($($expr:expr),*) => {
         let mut settings = insta::Settings::clone_current();
         settings.set_prepend_module_to_snapshot(false);
+        settings.set_snapshot_suffix("replies");
         let _guard = settings.bind_to_scope();
     };
 }
 
 #[tokio::test]
 #[serial]
-async fn test_model() {
+async fn test_create() {
     configure_insta!();
 
     let boot = boot_test::<App>().await.unwrap();
     seed::<App>(&boot.app_context).await.unwrap();
 
-    // query your model, e.g.:
-    //
-    // let item = models::posts::Model::find_by_pid(
-    //     &boot.app_context.db,
-    //     "11111111-1111-1111-1111-111111111111",
-    // )
-    // .await;
+    let reply = RepliesModel::create(
+        &boot.app_context.db,
+        "I've had some bad experiences with seagulls myself.".to_string(),
+        1,
+        2,
+        Some(1)
+    ).await.unwrap();
 
-    // snapshot the result:
-    // assert_debug_snapshot!(item);
+    assert_yaml_snapshot!(reply,
+        {
+            ".created_at" => "2025-09-04T22:17:46Z",
+            ".updated_at" => "2025-09-04T22:17:46Z",
+            ".last_active" => "2025-09-04T22:17:46.190572"
+        }
+    );
+}
+
+#[tokio::test]
+#[serial]
+async fn test_find_paginated_all() {
+    configure_insta!();
+
+    let boot = boot_test::<App>().await.unwrap();
+    seed::<App>(&boot.app_context).await.unwrap();
+
+    let threads = ReplisEntity::find_paginated(
+        &boot.app_context.db,
+        1,
+        2000,
+        0
+    ).await.unwrap();
+
+    assert_eq!(threads.len(), 2);
+
+    assert_yaml_snapshot!(threads,
+        {
+            ".created_at" => "2025-09-04T22:17:46Z",
+            ".updated_at" => "2025-09-04T22:17:46Z",
+            ".last_active" => "2025-09-04T22:17:46.190572"
+        }
+    );
+}
+
+#[tokio::test]
+#[serial]
+async fn test_find_paginated_second_page() {
+    configure_insta!();
+
+    let boot = boot_test::<App>().await.unwrap();
+    seed::<App>(&boot.app_context).await.unwrap();
+
+    let threads = ReplisEntity::find_paginated(
+        &boot.app_context.db,
+        1,
+        1,
+        1
+    ).await.unwrap();
+
+    assert_eq!(threads.len(), 1);
+    assert_eq!(threads[0].body, "Oh wow. That's terrible!".to_string());
+
+    assert_yaml_snapshot!(threads,
+        {
+            ".created_at" => "2025-09-04T22:17:46Z",
+            ".updated_at" => "2025-09-04T22:17:46Z",
+            ".last_active" => "2025-09-04T22:17:46.190572"
+        }
+    );
 }
--- tests/models/threads.rs
+++ tests/models/threads.rs
@@ -1,5 +1,9 @@
 use insta::{assert_debug_snapshot, assert_yaml_snapshot};
-use kboard::{app::App, models::threads::Model as ThreadsModel};
+use kboard::{
+    app::App,
+    models::threads::Model as ThreadsModel,
+    models::threads::Entity as ThreadsEntity
+};
 use loco_rs::testing::prelude::*;
 use serial_test::serial;
 
@@ -7,6 +11,7 @@ macro_rules! configure_insta {
     ($($expr:expr),*) => {
         let mut settings = insta::Settings::clone_current();
         settings.set_prepend_module_to_snapshot(false);
+        settings.set_snapshot_suffix("threads");
         let _guard = settings.bind_to_scope();
     };
 }
@@ -28,6 +33,88 @@ async fn test_create() {
     ).await.unwrap();
 
     assert_yaml_snapshot!(thread,
+        {
+            ".created_at" => "2025-09-04T22:17:46Z",
+            ".updated_at" => "2025-09-04T22:17:46Z",
+            ".last_active" => "2025-09-04T22:17:46.190572"
+        }
+    );
+}
+
+#[tokio::test]
+#[serial]
+async fn test_find_paginated_all() {
+    configure_insta!();
+
+    let boot = boot_test::<App>().await.unwrap();
+    seed::<App>(&boot.app_context).await.unwrap();
+
+    let threads = ThreadsEntity::find_paginated(
+        &boot.app_context.db,
+        1,
+        2000,
+        0
+    ).await.unwrap();
+
+    assert_eq!(threads.len(), 4);
+
+    assert_yaml_snapshot!(threads,
+        {
+            ".created_at" => "2025-09-04T22:17:46Z",
+            ".updated_at" => "2025-09-04T22:17:46Z",
+            ".last_active" => "2025-09-04T22:17:46.190572"
+        }
+    );
+}
+
+#[tokio::test]
+#[serial]
+async fn test_find_paginated_some() {
+    configure_insta!();
+
+    let boot = boot_test::<App>().await.unwrap();
+    seed::<App>(&boot.app_context).await.unwrap();
+
+    let threads = ThreadsEntity::find_paginated(
+        &boot.app_context.db,
+        1,
+        2,
+        0
+    ).await.unwrap();
+
+    assert_eq!(threads.len(), 2);
+    assert_eq!(threads[0].title, "Startled by a chihuahua.");
+    assert_eq!(threads[1].title, "Startled by a moose.");
+
+    assert_yaml_snapshot!(threads,
+        {
+            ".created_at" => "2025-09-04T22:17:46Z",
+            ".updated_at" => "2025-09-04T22:17:46Z",
+            ".last_active" => "2025-09-04T22:17:46.190572"
+        }
+    );
+}
+
+#[tokio::test]
+#[serial]
+async fn test_find_paginated_second_page() {
+    configure_insta!();
+
+    let boot = boot_test::<App>().await.unwrap();
+    seed::<App>(&boot.app_context).await.unwrap();
+
+    let threads = ThreadsEntity::find_paginated(
+        &boot.app_context.db,
+        1,
+        2,
+        1
+    ).await.unwrap();
+
+    assert_eq!(threads.len(), 2);
+    assert_eq!(threads[0].title, "Startled by a chipmunk.");
+    assert_eq!(threads[1].title, "Startled by a seagull.");
+
+    assert_yaml_snapshot!(threads,
         {
             ".created_at" => "2025-09-04T22:17:46Z",
             ".updated_at" => "2025-09-04T22:17:46Z",
